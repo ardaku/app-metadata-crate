c96bc9150ed20ae684e891eeef1f72d027665a04
Jeron Aldaron Lau <aldaronlau@gmail.com>
2022-06-19 18:28:50 -0500

    Prepare for initial v0.0.1 release

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+// Copyright © 2022 The Nucleide Contributors.
+//
+// Licensed under any of:
+// - Apache License, Version 2.0 (https://www.apache.org/licenses/LICENSE-2.0)
+// - Boost Software License, Version 1.0 (https://www.boost.org/LICENSE_1_0.txt)
+// - MIT License (https://mit-license.org/)
+// At your choosing (See accompanying files LICENSE_APACHE_2_0.txt,
+// LICENSE_MIT.txt and LICENSE_BOOST_1_0.txt).
+
 #![doc = include_str!("../README.md")]
 
 pub use parity_wasm::elements::CustomSection;
@@ -19,17 +28,28 @@ impl Module {
     }
 
     /// Returns an iterator over the module’s custom sections.
-    pub fn custom_sections(&self) -> impl Iterator<Item = &elements::CustomSection> {
+    pub fn custom_sections(
+        &self,
+    ) -> impl Iterator<Item = &elements::CustomSection> {
         self.0.custom_sections()
     }
 
-    /// Sets the payload associated with the given custom section, or adds a new custom section, as appropriate.
-    pub fn add_custom_section(&mut self, name: impl Into<String>, payload: Vec<u8>) {
+    /// Sets the payload associated with the given custom section, or adds a new
+    /// custom section, as appropriate.
+    pub fn add_custom_section(
+        &mut self,
+        name: impl Into<String>,
+        payload: Vec<u8>,
+    ) {
         self.0.set_custom_section(name, payload)
     }
 
-    /// Removes the given custom section, if it exists. Returns the removed section if it existed, or None otherwise.
-    pub fn clear_custom_section(&mut self, name: impl AsRef<str>) -> Option<CustomSection> {
+    /// Removes the given custom section, if it exists. Returns the removed
+    /// section if it existed, or None otherwise.
+    pub fn clear_custom_section(
+        &mut self,
+        name: impl AsRef<str>,
+    ) -> Option<CustomSection> {
         self.0.clear_custom_section(name)
     }
 
